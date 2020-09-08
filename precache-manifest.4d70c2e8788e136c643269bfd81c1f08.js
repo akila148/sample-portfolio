73a5fb12845ed18ed834ea6a3d471cef186af5c0
Akila <akila@Akilas-MacBook-Air.local>
2020-09-08 14:53:57 +0530

    fisrt commit

---
+++ precache-manifest.4d70c2e8788e136c643269bfd81c1f08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35902960bdfbc2b5befadadefbff29da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd6556e462008eef15f6",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd6556e462008eef15f6",
+    "url": "/static/js/main.165ce25a.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
